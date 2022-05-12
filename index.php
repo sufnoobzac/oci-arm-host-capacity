81e9da94fdfee35cf4e8dad46ebcd3589bc784b5
sufnoobzac <yusufpraditya@gmail.com>
2022-05-12 13:12:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ $config = new OciConfig(
     getenv('OCI_TENANCY_ID'),
     getenv('OCI_KEY_FINGERPRINT'),
     getenv('OCI_PRIVATE_KEY_FILENAME'),
-    getenv('OCI_AVAILABILITY_DOMAIN') ?: null, // null or '' or 'jYtI:PHX-AD-1' or ['jYtI:PHX-AD-1','jYtI:PHX-AD-2']
+    'OwqN:AP-SINGAPORE-1-AD-1', // null or '' or 'jYtI:PHX-AD-1' or ['jYtI:PHX-AD-1','jYtI:PHX-AD-2']
     getenv('OCI_SUBNET_ID'),
     getenv('OCI_IMAGE_ID'),
     (int) getenv('OCI_OCPUS'),
